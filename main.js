14ee02048ae0d1e93642f33c9af5c0ba3ef8e15e
SebSpr <sebastian.spreitzer@uibk.ac.at>
2024-04-18 15:06:42 +0200

    scalebar

--- main.js
+++ main.js
@@ -12,6 +12,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+L.control.scale({
+  imperial: false,
+  maxWidth: 150
+}).addTo(map);
 
 var jsondata =  {
     "type": "FeatureCollection",
